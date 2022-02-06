b0d98a0db729f8e6aea7bc09bef131e376d83b66
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-06 10:32:11 +0500

    Implementation
    Implement javascript and css in svelte file.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let count = 0;
+
+    function incrementBtn (){
+        count +=1
+    }
+</script>
+
+<h1>Number : {count}</h1>
+<button class="btn" on:click={incrementBtn}>Increment</button>
+<!-- <p>Visit <a href="/about">kit.svelte.dev</a> to read the documentation</p> -->
+
+<style>
+  h1{
+      font-size: 70px;
+  }
+</style>
