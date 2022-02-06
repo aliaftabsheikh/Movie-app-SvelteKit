b3198f2419a09549af0d3bdb4bc822707117fe78
Ali Aftab Sheikh <aliaftabsheikh1@gmail.com>
2022-02-06 12:33:00 +0500

    Movie Card
    Complete Movie Card.

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -4,9 +4,35 @@
 
 <div class="movie-card">
     <img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt={movie.title}>
+    <div class="description">
+        <h2>{movie.title}</h2>
+        <p>{movie.release_date}</p>
+    </div>
 </div>
 
-<div class="description">
-    <h2>{movie.title}</h2>
-    <p>{movie.release_date}</p>
-</div>
+
+<style>
+    img{
+        width: 100%;
+        height: 50vh;
+        object-fit: cover;
+        border-radius: 1rem;
+        margin-bottom: 1rem;
+    }
+    h2{
+        font-size: 0.9rem;
+    }
+    .description{
+        height: 5vh;
+    }
+    p{
+        font-size: 0.7rem;
+    }
+    .movie-card{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        padding: 1rem;
+
+    }
+</style>
--- src/components/PopularMovie.svelte
+++ src/components/PopularMovie.svelte
@@ -10,3 +10,15 @@
     <MovieCard {movie}/>
     {/each}
 </div>
+
+<style>
+    h3{
+        padding: 0 1rem;
+    }
+    .popular-movies{
+        display: grid;
+        grid-template-columns:repeat(auto-fit, minmax(250px, 1fr));
+        grid-column-gap: 1rem;
+        grid-row-gap: 2rem;
+    }
+</style>
